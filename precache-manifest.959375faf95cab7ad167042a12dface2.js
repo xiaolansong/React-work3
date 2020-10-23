a628350fef5db4bc2a9502d18dfadc932e173d8d
xiaolansong <907499926@qq.com>
2020-10-23 12:06:26 +0800

    upload

---
+++ precache-manifest.959375faf95cab7ad167042a12dface2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186bb96bafd99466a943467468cda516",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c65a219f1a3c4b0d62f",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "6c65a219f1a3c4b0d62f",
+    "url": "/static/js/2.04239ee6.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.04239ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb09ba08be9bf0ed6be8",
+    "url": "/static/js/main.32c9da2f.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
